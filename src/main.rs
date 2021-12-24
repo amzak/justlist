38b9dea3f6da266cc887900e481a29e6d4643aa4
Andrey Zak <amzak.only@gmail.com>
2021-12-24 23:14:29 +0300

    Basic cursive hello world app

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use cursive::views::TextView;
+use cursive::Cursive;
+
 fn main() {
-    println!("Hello, world!");
+    let mut siv = cursive::default();
+
+    siv.add_global_callback('q', Cursive::quit);
+
+    siv.add_fullscreen_layer(TextView::new(
+        "Hello World!\n\
+         Press q to quit the application.",
+    ));
+
+    siv.run();
 }
