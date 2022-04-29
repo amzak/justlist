2c572524ee0e30da46c2d40b5cd9900032778e7a
Andrey Zak <amzak.only@gmail.com>
2022-04-30 00:02:51 +0300

    Added search for files by name and directories

--- src/plugins/search/main.rs
+++ src/plugins/search/main.rs
@@ -4,14 +4,45 @@ use shared::serialization::*;
 use std::env;
 use std::ffi::OsStr;
 use std::io::BufReader;
+use std::path::Path;
 use std::path::PathBuf;
 use structopt::StructOpt;
 use walkdir::WalkDir;
+
+#[derive(Debug)]
+struct QueryFlags {
+    names: bool,
+    directories: bool,
+    extensions: bool,
+}
+
+fn parse_query_flags(s: &str) -> QueryFlags {
+    let mut names = s.len() == 0; // if nothing was specified, search for names
+    let mut directories = false;
+    let mut extensions = false;
+
+    for c in s.chars() {
+        match c {
+            'n' => names = true,
+            'd' => directories = true,
+            'e' => extensions = true,
+            _ => {}
+        }
+    }
+
+    QueryFlags {
+        names,
+        directories,
+        extensions,
+    }
+}
 
 #[derive(Debug, StructOpt)]
 struct Options {
     query: String,
     command_template: String,
+    #[structopt(short, parse(from_str = parse_query_flags))]
+    query_flags: QueryFlags,
     #[structopt(long, short)]
     verbose: bool,
     #[structopt(long, short, default_value = "1")]
@@ -20,6 +51,29 @@ struct Options {
     working_dir: Option<PathBuf>,
     #[structopt(long, short)]
     title: Option<String>,
+}
+
+fn is_match(query: &str, path: &Path, query_flags: &QueryFlags) -> bool {
+    let is_dir = path.is_dir();
+    let mut result = false;
+    let file_name = path.file_name().unwrap().to_str().unwrap();
+
+    if is_dir && query_flags.directories {
+        result |= file_name.contains(query);
+    }
+
+    if query_flags.extensions && !is_dir {
+        if let Some(extension_wrap) = path.extension() {
+            let extension = extension_wrap.to_str().unwrap();
+            result |= extension.contains(query);
+        }
+    }
+
+    if query_flags.names && !is_dir {
+        result |= file_name.contains(query);
+    }
+
+    result
 }
 
 fn main() -> std::io::Result<()> {
@@ -60,20 +114,8 @@ fn main() -> std::io::Result<()> {
 
         let dir_item = item.unwrap();
         let path = dir_item.path();
-        let extension = path.extension();
 
-        if path.is_dir() {
-            continue;
-        }
-
-        if extension.is_none() {
-            continue;
-        }
-
-        let ext = extension.unwrap();
-        let filter_ext = OsStr::new(&options.query);
-
-        if filter_ext == ext {
+        if is_match(&options.query, path, &options.query_flags) {
             let file_name = path.file_name().unwrap();
             let file_path = path.to_str().unwrap();
 
