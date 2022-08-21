91aa5fd46926a1ed42a98bd870b16628eb954b10
Andrey Zak <amzak.only@gmail.com>
2022-08-21 22:51:24 +0300

    Git  repo plugin refactored with plugin trait

--- src/plugins/git-repo/main.rs
+++ src/plugins/git-repo/main.rs
@@ -1,14 +1,12 @@
-use atty::Stream;
-use serde::de::Deserialize;
+use shared::plugin::{JustListAction, JustListPlugin};
 use shared::serialization::*;
 use std::env;
-use std::io::BufReader;
 use std::path::{Path, PathBuf};
 use structopt::StructOpt;
 use walkdir::WalkDir;
 
 #[derive(Debug, StructOpt)]
-#[structopt(about = "This plugin searches git repos by looking for .git folder")]
+#[structopt(about = "This plugin searches for git repos")]
 struct Options {
     command_template: String,
     #[structopt(long, short, default_value = "1")]
@@ -20,9 +18,54 @@ struct Options {
     #[structopt(
         long = "terminal",
         short = "t",
-        help = "This flag should indicate, that the app runs in a terminal"
+        help = "This flag indicates, that the current terminal should be reused"
     )]
     is_terminal: bool,
+}
+
+struct SearchGitRepos {}
+
+impl JustListAction<Options> for SearchGitRepos {
+    fn execute(&self, groups: &mut Groups, options: &Options) {
+        let cwd = env::current_dir().unwrap();
+        let working_dir = options.working_dir.as_ref().unwrap_or(&cwd);
+        let depth = options.depth;
+
+        let mut group = ListGroup {
+            label: "git repos".to_string(),
+            items: vec![],
+            command_template: Some(options.command_template.to_string()),
+            is_terminal: Some(options.is_terminal),
+        };
+
+        for item in WalkDir::new(&working_dir).max_depth(depth as usize) {
+            if let Err(_error) = item {
+                if options.verbose {
+                    eprintln!("{}", _error);
+                }
+                continue;
+            }
+
+            let dir_item = item.unwrap();
+            let path = dir_item.path();
+
+            if !is_git_repo(path) {
+                continue;
+            }
+
+            if let Some(parent_dir) = path.parent() {
+                let file_path = path.to_str().unwrap();
+                let dir_name = parent_dir.file_name().unwrap().to_str().unwrap();
+
+                group.items.push(SelectableItem {
+                    label: String::from(dir_name),
+                    param: String::from(file_path),
+                })
+            }
+        }
+
+        groups.groups.push(group);
+    }
 }
 
 fn is_git_repo(path: &Path) -> bool {
@@ -35,58 +78,8 @@ fn is_git_repo(path: &Path) -> bool {
 fn main() -> std::io::Result<()> {
     let options = Options::from_args();
 
-    let working_dir = options.working_dir.unwrap_or(env::current_dir().unwrap());
-    let depth = options.depth;
+    let plugin = JustListPlugin::new(options);
 
-    let stdin = std::io::stdin();
-    let handle = stdin.lock();
-    let reader = BufReader::new(handle);
-
-    let mut groups: Groups = Groups { groups: vec![] };
-    if atty::isnt(Stream::Stdin) {
-        let mut de = serde_json::Deserializer::from_reader(reader);
-        groups = Groups::deserialize(&mut de).unwrap();
-    }
-
-    let mut group = ListGroup {
-        label: "git repos".to_string(),
-        items: vec![],
-        command_template: Some(options.command_template),
-        is_terminal: Some(options.is_terminal),
-    };
-
-    for item in WalkDir::new(&working_dir).max_depth(depth as usize) {
-        if let Err(_error) = item {
-            if options.verbose {
-                eprintln!("{}", _error);
-            }
-            continue;
-        }
-
-        let dir_item = item.unwrap();
-        let path = dir_item.path();
-
-        if !is_git_repo(path) {
-            continue;
-        }
-
-        if let Some(parent_dir) = path.parent() {
-            let file_path = path.to_str().unwrap();
-            let dir_name = parent_dir.file_name().unwrap().to_str().unwrap();
-
-            group.items.push(SelectableItem {
-                label: String::from(dir_name),
-                param: String::from(file_path),
-            })
-        }
-    }
-
-    groups.groups.push(group);
-
-    let stdout = std::io::stdout();
-    let stdout_handle = stdout.lock();
-    let writer = std::io::BufWriter::new(stdout_handle);
-    serde_json::to_writer(writer, &groups)?;
-
-    Ok(())
+    let action = SearchGitRepos {};
+    plugin.main(&action)
 }
